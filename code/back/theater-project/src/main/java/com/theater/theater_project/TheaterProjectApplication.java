e9e392ed806f8b93ca51f2abcc3a0d6b8b58b024
mmhqs <112990361+mmhqs@users.noreply.github.com>
2025-09-06 13:34:35 -0300

    feat: create spring project

---
+++ code/back/theater-project/src/main/java/com/theater/theater_project/TheaterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.theater.theater_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterProjectApplication.class, args);
+	}
+
+}
---
+++ code/back/theater-project/src/test/java/com/theater/theater_project/TheaterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theater.theater_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
